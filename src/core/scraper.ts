a67660123f81e05f263575f36432b7094e93f47c
aryasakshi13 <sakshiarya884@gmail.com>
2026-05-20 16:28:31 +0530

    Impleted rgb to hex covert logic

--- src/core/scraper.ts
+++ src/core/scraper.ts
@@ -1,35 +1,50 @@
-console.log("The scraper script has been successfully injected");
+// console.log("The scraper script has been successfully injected");
 
-chrome.runtime.onMessage.addListener(
-    (
-        message: {action: string}, 
-        sender: chrome.runtime.MessageSender, 
-        sendResponse:(response: any)=> void)=>{
+// chrome.runtime.onMessage.addListener(
+//     (
+//         message: {action: string}, 
+//         sender: chrome.runtime.MessageSender, 
+//         sendResponse:(response: any)=> void)=>{
 
-         if(message.action === "PING_DOM"){
-            console.log("POPup clicked ! Analyzing the page backhround color...");
+//          if(message.action === "PING_DOM"){
+//             console.log("POPup clicked ! Analyzing the page backhround color...");
 
-            const bodyStyles = window.getComputedStyle(document.body);
-            let actualColor = bodyStyles.backgroundColor ;
+//             const bodyStyles = window.getComputedStyle(document.body);
+//             let actualColor = bodyStyles.backgroundColor ;
 
 
-          if(actualColor === "rgba(0, 0, 0, 0)" || actualColor === "transparent"){
-             const rootStyles = window.getComputedStyle(document.documentElement);
-             actualColor = rootStyles.backgroundColor;
-          }
+//           if(actualColor === "rgba(0, 0, 0, 0)" || actualColor === "transparent"){
+//              const rootStyles = window.getComputedStyle(document.documentElement);
+//              actualColor = rootStyles.backgroundColor;
+//           }
 
-          if (actualColor === "rgba(0, 0, 0, 0)" || actualColor === "transparent") {
-                // Check if the user's system or site prefers a dark layout
-                const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-                actualColor = isDarkMode ? "rgb(15, 23, 42)" : "rgb(255, 255, 255)";
-            }
+//           if (actualColor === "rgba(0, 0, 0, 0)" || actualColor === "transparent") {
+//                 // Check if the user's system or site prefers a dark layout
+//                 const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+//                 actualColor = isDarkMode ? "rgb(15, 23, 42)" : "rgb(255, 255, 255)";
+//             }
         
              
-        sendResponse({
-            status: "success",
-            backgroundColor: actualColor
-        });
-     }
+//         sendResponse({
+//             status: "success",
+//             backgroundColor: actualColor
+//         });
+//      }
 
-     return true ;
-});
+//      return true ;
+// });
+
+console.log("Deep Palate Scraper Injected!");
+function rgbToHex(rgbStr: string): string | null {
+    const match = rgbStr.match(/\d+/g);
+
+    if(!match || match.length <3 )  return null ;
+
+    if(match.length ===4 && parseFloat(match[3])=== 0) return null ;
+
+    const r = parseInt(match[0]).toString(16).padStart(2, '0');
+     const g = parseInt(match[1]).toString(16).padStart(2, '0');
+      const b= parseInt(match[2]).toString(16).padStart(2, '0');
+
+     return '#${r}${g}${b}'.toLowerCase();
+}
