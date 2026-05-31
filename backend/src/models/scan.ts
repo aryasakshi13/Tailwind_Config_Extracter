47d63c132244ad10a4b2c4de60c807ff17087e1e
aryasakshi13 <sakshiarya884@gmail.com>
2026-05-31 17:08:31 +0530

    Impleted ThemeSchema

---
+++ backend/src/models/scan.ts
@@ -0,0 +1,48 @@
+import {Schema, model, Document} from 'mongoose';
+
+ 
+interface IScan extends Document {
+    userId: Schema.Types.ObjectId;
+    siteUrl: string;
+    siteName: string;
+    sections: {
+        [sectionName: string]: {
+            colors: string[];
+            fonts: string[];
+            space: string[];
+
+        }; 
+    };
+}
+
+const ThemeSchema = new Schema<IScan>(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+
+        },
+        siteUrl :{
+             type: String,
+             required: true
+        },
+        siteName:{
+            type: String
+        },
+        
+        sections : {
+            type: Map,
+            of: new Schema({
+                colors: [String],
+                fonts: [String],
+                spaces: [String]
+            },{id:false})
+
+        }
+    },{
+        timestamps: true
+    }
+);
+
+export const Theme = model<IScan>('Theme', ThemeSchema);
